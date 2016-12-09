ac01a4ef1a7002dc442ffa99be868ddcb081e601
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-09 10:30:15 -0800

    Page 153 Complete

---
+++ SimpleClassExample/SimpleClassExample/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * C# 6.0 and the .NET 4.6 Framework" by Andrew Troelsen & Philip Japikse
+ * Pg 151 - Understanding Encapsulation
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Class Types *****\n");
+
+            // Allocate and configure a Car object.
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currSpeed = 10;
+
+            // Speed up the car a few times and print new state.
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
