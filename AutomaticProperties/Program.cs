b4fff62d6cbae0ae282fa3baf5feaa61abb798da
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-10 13:15:39 -0800

    interacting with automatic properties

--- AutomaticProperties/Program.cs
+++ AutomaticProperties/Program.cs
@@ -11,7 +11,32 @@
 	 {
 		 static void Main(string[] args)
 		 {
-			 Console.WriteLine("Automatic Properties Start");
+			 Console.WriteLine("Automatic Properties");
+			 
+			 // Create object
+			 Car c = new Car();
+			 c.PetName = "Frank";
+			 c.Speed = 55;
+			 c.Color = "Red";
+			 
+			 c.DisplayStats();
+			 
+			 Console.ReadLine();
+		 }
+	 }
+	 
+	 class Car
+	 {
+		 // Automatic properties
+		 public string PetName {get; set; }
+		 public int Speed {get; set; }
+		 public string Color {get; set; }
+		 
+		 public void DisplayStats()
+		 {
+			 Console.WriteLine("Car Name: {0}", PetName);
+			 Console.WriteLine("Speed: {0}", Speed);
+			 Console.WriteLine("Color: {0}", Color);
 		 }
 	 }
  }
