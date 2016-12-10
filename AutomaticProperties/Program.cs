2b0e3e73b3510e2a3be440a91c73ab822703fc0f
Nathan Connors <nathanaeljconnors@gmail.com>
2016-12-10 13:09:28 -0800

    automatic properties start

---
+++ AutomaticProperties/Program.cs
@@ -0,0 +1,17 @@
+/*
+ * C# 6.0 and the .NET 4.6 Framework" by Andrew Troelsen & Philip Japikse
+ * Pg 192 - Automatic Properties
+ */
+ 
+ using System;
+ 
+ namespace AutomaticProperties
+ {
+	 class Program
+	 {
+		 static void Main(string[] args)
+		 {
+			 Console.WriteLine("Automatic Properties Start");
+		 }
+	 }
+ }
