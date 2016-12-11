5bc1223e562133395b89175ac50f4654b7530f64
James Connors <James Connors>
2016-12-11 15:02:07 -0800

    car class addition

---
+++ Inheritance/Inheritance/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Car
+    {
+        public readonly int maxSpeed;
+        private int currSpeed;
+
+		// default constructor.
+		public Car()
+        {
+            maxSpeed = 55;
+        }
+
+		// constructors.
+		public Car(int max)
+        {
+            maxSpeed = max;
+        }
+
+		public int speed
+        {
+			get { return currSpeed; }
+            set
+            {
+                currSpeed = value;
+				if(currSpeed > maxSpeed)
+                {
+                    currSpeed = maxSpeed;
+                }
+            }
+        }
+    }
+}
