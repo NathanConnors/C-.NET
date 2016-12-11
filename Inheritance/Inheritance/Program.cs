ae4a2bb2651dc2780448dc46bcdb4b676034b3a6
James Connors <James Connors>
2016-12-11 14:58:21 -0800

    addition of car class

--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * C# 6.0 and the .NET 4.6 Framework" by Andrew Troelsen & Philip Japikse
+ * Pg 205 - Inheritance
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
